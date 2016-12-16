adce08e63ef66b6aad25cfc918016414cf4ac9d7
kamfor <kfforex@gmail.com>
2016-12-16 18:21:40 +0100

    modprobe

--- scripts/initial.sh
+++ scripts/initial.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-/sbin/modporbe -r pru_rproc
+/sbin/modprobe -r pru_rproc
 sleep .3
-/sbin/modporbe  pru_rproc
+/sbin/modprobe  pru_rproc
 
 if [ ! -d /sys/class/gpio/gpio78 ]; then echo 78 > /sys/class/gpio/export; fi
 if [ ! -d /sys/class/gpio/gpio79 ]; then echo 79 > /sys/class/gpio/export; fi
