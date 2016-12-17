0b0630a56b748cf5033e732822d7b8ef940e23a3
michalrom089 <michal.rom089@gmail.com>
2016-12-17 12:41:16 +0100

    Led trigger off.

--- scripts/initial.sh
+++ scripts/initial.sh
@@ -15,3 +15,8 @@ echo out > /sys/class/gpio/gpio79/direction
 echo 1 > /sys/class/gpio/gpio78/value
 echo 1 > /sys/class/gpio/gpio79/value
 
+echo none > /sys/class/leds/beaglebone:green:usr0/trigger
+echo none > /sys/class/leds/beaglebone:green:usr1/trigger
+echo none > /sys/class/leds/beaglebone:green:usr2/trigger
+echo none > /sys/class/leds/beaglebone:green:usr3/trigger
+
