cd547195546e36f0188263e06ef89a1c06b0737f
michal.rom089 <michal.rom089@gmail.com>
2017-03-23 22:08:39 +0100

    Upgraded config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,28 @@
+#!/bin/bash
+
+rys_ip=$1
+
+if [[ $rys_ip == "" ]] ; then
+	echo 'Invalid parameters. Pleas pass ip address like this : ./config.sh ip_address '
+	exit 1
+fi
+
+ssh -q root@$rys_ip exit 
+if [[ $(echo $?) == 0 ]] ; then
+	echo Host is reachable.
+else
+	echo Host cannot be reached
+	exit 2
+fi
+
+# -t is for allocating TTY
+#TTY - text-only console
+ssh -t -t root@$rys_ip '/bin/bash' << EOF
+
 git clone https://github.com/GroupOfRobots/RobotConfig
 
 cd ~/RobotConfig
-nano /boot/uEnv.txt add line dtb=am335x-bonegreen-wireless.dtb
+echo dtb=am335x-bonegreen-wireless.dtb >> /boot/uEnv.txt
 cp DevTree/am335x-bonegreen-wireless.dts /opt/source/dtb-4.4-ti/src/arm
 cp DevTree/am335x-bonegreen-irys.dtsi /opt/source/dtb-4.4-ti/src/arm
 
@@ -21,10 +42,13 @@ cp scripts/startup.service /etc/systemd/system
 
 chmod 744 /usr/bin/initial.sh
 chmod 664 /etc/systemd/system/startup.service
+
+
 systemctl daemon-reload
 systemctl enable startup.service
 systemctl start startup.service
 
+echo Disabling services
 systemctl stop apache2.service
 systemctl disable apache2.service
 
@@ -37,6 +61,9 @@ systemctl disable cloud9.socket
 systemctl stop wificonfig.service
 systemctl disable wificonfig.service
 
-apt-get remove nodejs
+reboot
+exit 0
+#sudo apt-get remove nodejs
+EOF
 
 
