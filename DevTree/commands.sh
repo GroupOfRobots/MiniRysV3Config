355dd0f6faf69ade183c71af2f6b2409678dbc2b
mjbogusz <mjbogusz+github@gmail.com>
2017-06-12 19:35:10 +0200

    device tree: re-added SPI pins and declarations (no longer in .dtsi file in 4.9-ti branch)

--- DevTree/commands.sh
+++ DevTree/commands.sh
@@ -1,17 +1,7 @@
-#edit uEnv.txt add line dtb=am335x-bonegreen-wireless.dtb
-cp am335x-bonegreen-wireless.dts /opt/source/dtb-4.4-ti/src/arm
-cp am335x-bonegreen-irys.dtsi /opt/source/dtb-4.4-ti/src/arm
+#edit /boot/uEnv.txt: add line dtb=am335x-bonegreen-wireless.dtb
+cp am335x-bonegreen-wireless.dts /opt/source/dtb-4.9-ti/src/arm
+cp am335x-bonegreen-irys.dtsi /opt/source/dtb-4.9-ti/src/arm
 
-cd /opt/source/dtb-4.4-ti 
-make 
-make install
-
-#compile dev-tree BB_BONE_IRYS to enable ADC and SPI 
-
-dtc -O dtb -o BB_BONE_IRYS.dtbo -b 0 -@ BB_BONE_IRYS.dts
-#move compiled to /lib/firmware and echo 
-echo BB-BONE-IRYS > /sys/devices/platform/bone_capemgr/slots
-
-#lub samo adc 
-
-echo BB-ADC > /sys/devices/platform/bone_capemgr/slots
+cd /opt/source/dtb-4.9-ti
+make
+sudo make install
